51e586fbfef11194c4c854b95d5354d03de9d10e
LIZXP <Peter.xli@outlook.com>
2022-09-05 21:16:46 -0600

    convert App function from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
-import "./App.css";
-import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
+import { useState, useEffect } from "react";
 
-class App extends Component {
+const App = () => {
+  const [searchField, setSearchField] = useState(""); //[value,setValue] inside useState
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+  //useEffect takes in 2 varrables, first is a callback, will run when the render mounts the first time, second one is array. the callback runs again eveytime when the value in the array changes.
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((users) => {
+        return setMonsters(users);
+      });
+  }, []);
+  //for this useEffect, we set the useState initially as monsters, and passed in monsters and searchField to the 2nd varriable. when ever they changed, the first callback function will triggered.
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Robot Dome</h1>
+      <SearchBox
+        className="search-box"
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+{
+  /* class App extends Component {
   constructor() {
     super();
     this.state = {
@@ -37,17 +76,10 @@ class App extends Component {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
     return (
-      <div className="App">
-        <h1 className="app-title">Robot Dome</h1>
-        <SearchBox
-          className="search-box"
-          onChangeHandler={onSearchChange}
-          placeholder="search monsters"
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
+      
     );
   }
+} */
 }
 
 export default App;
